36536471620fa99e770048d9abb4b61e35b23b9c
gao_yangkai <zard200107@gmail.com>
2025-05-17 23:56:57 +0800

    metadata

---
+++ src/constants/metadata.ts
@@ -0,0 +1,17 @@
+const DEFAULT_METADATA = {
+  title: "Izumi0uu Blog",
+  description: "Izumi0uu Blog",
+  image: "/og-image.png",
+};
+
+const PAGE_METADATA = {
+  "": {
+    title: "Home",
+    description: "Home",
+    image: "/og-image.png",
+  },
+};
+
+const titleSeparator = "|";
+
+export { DEFAULT_METADATA, PAGE_METADATA, titleSeparator };
---
+++ src/utils/content/metadata.ts
@@ -0,0 +1,5 @@
+import {
+  DEFAULT_METADATA,
+  PAGE_METADATA,
+  titleSeparator,
+} from "@/constants/metadata";
