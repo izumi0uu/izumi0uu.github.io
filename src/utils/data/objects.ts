6957f82bd91c20eddef5cacea1a578a6849fe610
gao_yangkai <zard200107@gmail.com>
2025-05-22 17:59:49 +0800

    utils data objects

--- src/utils/data/objects.ts
+++ src/utils/data/objects.ts
@@ -21,4 +21,22 @@ const getValueFromFunctionOrFallback = <T>(
     return fallbackValue;
 }
 
-export { getValueFromFunctionOrFallback };
+/**
+ * @description filter undefined values from an object
+ * @returns {T} object with undefined values removed
+ */
+const filterUndefined = <T extends Record<string, any>>(obj: T): T =>
+    Object.fromEntries(Object.entries(obj).filter(([_key, value]) => value !== undefined)) as T;
+  
+/**
+ * @description randomize an array
+ * @returns {T[]} array with items randomized
+ */
+const randomizeArray = <T>(array: T[]): T[] =>
+    array
+      .map((item) => ({ item, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ item }) => item);
+  
+
+export { getValueFromFunctionOrFallback, filterUndefined, randomizeArray };
