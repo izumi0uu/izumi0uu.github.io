445f68709e096ac2b89eb90f8cc1c871355aa740
gao_yangkai <zard200107@gmail.com>
2025-05-25 16:26:26 +0800

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,6 @@
+/**
+ * @description in SSG pattern, the main effect of middleware is to prepare data for building and simulate the behavior of the development server.
+ */
 export function onRequest(context: any, next: () => Promise<void>) {
   // intercept data from a request
   // optionally, modify the properties in `locals`
