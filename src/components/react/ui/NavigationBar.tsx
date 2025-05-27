fb1302d4e7d14701c775f9dba4ef395e0a349586
gao_yangkai <zard200107@gmail.com>
2025-05-27 18:22:16 +0800

    nav bar

--- src/components/react/ui/NavigationBar.tsx
+++ src/components/react/ui/NavigationBar.tsx
@@ -15,6 +15,7 @@ import { ROUTES } from "@/constants/routes"
 import * as m from "@/paraglide/messages"
 
 import { cn } from "@/utils/ui/styles"
+import { Link } from "@radix-ui/react-navigation-menu"
 // import { Icons } from "@/components/icons"
 
 const components: { title: string; href: string; description: string }[] = [
@@ -56,81 +57,41 @@ const components: { title: string; href: string; description: string }[] = [
 
 const NavigationBar = () => {
   return (
-    <NavigationMenu className={cn("bg-surface-container", "border-secondary")}>
+    <NavigationMenu>
       <NavigationMenuList className="flex flex-col items-center justify-center space-x-4 md:flex-row">
         <NavigationMenuItem>
-          <NavigationMenuTrigger>{m["pages.lists.blog.title"]()}</NavigationMenuTrigger>
+          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
           <NavigationMenuContent>
-            <ul
-              className={cn(
-                "bg-surface-container-low grid w-full gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]"
-              )}
-            >
+            <ul className="grid w-full gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
               <li className="row-span-3">
                 <NavigationMenuLink asChild>
                   <a
-                    className={cn(
-                      "bg-surface-variant hover:bg-surface-container-high flex size-full flex-col justify-end rounded-md p-6 no-underline transition-all duration-300 outline-none select-none focus:shadow-md",
-                      "hover:bg-surface-container-high-dark"
-                    )}
+                    className="from-muted/50 to-muted flex size-full flex-col justify-end rounded-md bg-gradient-to-b p-6 no-underline outline-none select-none focus:shadow-md"
                     href="/"
                   >
                     {/* <Icons.logo className="h-6 w-6" /> */}
-                    <div
-                      className={cn(
-                        "text-headings mt-4 mb-2 text-lg font-medium",
-                        "hover:text-headings"
-                      )}
-                    >
-                      BUOUUI
-                    </div>
-                    <p
-                      className={cn(
-                        "text-content-secondary text-sm leading-tight",
-                        "hover:text-content"
-                      )}
-                    >
+                    <div className="mt-4 mb-2 text-lg font-medium">BUOUUI</div>
+                    <p className="text-muted-foreground text-sm leading-tight">
                       Beautifully designed components that you can copy and paste into your apps.
                       Accessible. Customizable. Open Source.
                     </p>
                   </a>
                 </NavigationMenuLink>
               </li>
-              <ListItem
-                href="/docs"
-                title="Introduction"
-                className={cn(
-                  "bg-surface-container-high text-content-secondary",
-                  "hover:bg-surface-container-high-dark"
-                )}
-              >
+              <ListItem href="/docs" title="Introduction">
                 Re-usable components built using Radix UI and Tailwind CSS.
               </ListItem>
-              <ListItem
-                href="/docs/installation"
-                title="Installation"
-                className={cn(
-                  "bg-surface-container-high text-content-secondary",
-                  "hover:bg-surface-container-high-dark"
-                )}
-              >
+              <ListItem href="/docs/installation" title="Installation">
                 How to install dependencies and structure your app.
               </ListItem>
-              <ListItem
-                href="/docs/primitives/typography"
-                title="Typography"
-                className={cn(
-                  "bg-surface-container-high text-content-secondary",
-                  "hover:bg-surface-container-high-dark"
-                )}
-              >
+              <ListItem href="/docs/primitives/typography" title="Typography">
                 Styles for headings, paragraphs, lists...etc
               </ListItem>
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>{m["pages.lists.projects.title"]()}</NavigationMenuTrigger>
+          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid w-full max-w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
               {components.map((component) => (
@@ -142,18 +103,11 @@ const NavigationBar = () => {
           </NavigationMenuContent>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <a href="/docs">
+          <Link href="/docs">
             <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "border-0 uppercase")}>
-              {m["pages.lists.experience.title"]()}
+              Documentation
             </NavigationMenuLink>
-          </a>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <a href="/docs">
-            <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "border-0 uppercase")}>
-              {m["pages.about.title"]()}
-            </NavigationMenuLink>
-          </a>
+          </Link>
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
@@ -168,13 +122,13 @@ const ListItem = React.forwardRef<React.ElementRef<"a">, React.ComponentPropsWit
           <a
             ref={ref}
             className={cn(
-              "hover:bg-surface-container block space-y-1 rounded-md p-3 leading-none no-underline transition-all duration-300 outline-none select-none",
+              "block space-y-1 rounded-md p-3 leading-none no-underline transition-colors outline-none select-none",
               className
             )}
             {...props}
           >
-            <div className="text-headings text-sm leading-none font-medium uppercase">{title}</div>
-            <p className="text-content-secondary line-clamp-2 text-sm leading-snug">{children}</p>
+            <div className="text-sm leading-none font-medium uppercase">{title}</div>
+            <p className="text-muted-foreground line-clamp-2 text-sm leading-snug">{children}</p>
           </a>
         </NavigationMenuLink>
       </li>
