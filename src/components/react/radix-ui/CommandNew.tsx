3ac12ec515e82cde0dbf0026198cd6b7ca0c079c
gao_yangkai <zard200107@gmail.com>
2025-05-30 16:27:18 +0800

    command component

--- src/components/react/radix-ui/CommandNew.tsx
+++ src/components/react/radix-ui/CommandNew.tsx
@@ -135,7 +135,7 @@ CommandItem.displayName = CommandPrimitive.Item.displayName
 const CommandShortcut = ({ className, ...props }: React.HTMLAttributes<HTMLSpanElement>) => {
   return (
     <span
-      className={cn("text-muted-foreground ml-auto text-xs tracking-widest", className)}
+      className={cn("text-on-surface-variant ml-auto text-xs tracking-widest", className)}
       {...props}
     />
   )
