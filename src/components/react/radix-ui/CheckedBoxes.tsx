a091704bc2e6039fb2a908cfb1e1f44d4fc0521e
gao_yangkai <zard200107@gmail.com>
2025-06-01 15:27:43 +0800

    checkedBoxes

---
+++ src/components/react/radix-ui/CheckedBoxes.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import * as React from "react";
+import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu";
+
+import { Button } from "@/components/react/radix-ui/Button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/react/radix-ui/DropdownMenu";
+
+type Checked = DropdownMenuCheckboxItemProps["checked"];
+
+const DropdownMenuCheckboxes: React.FC = () => {
+  const [showStatusBar, setShowStatusBar] = React.useState<Checked>(true);
+  const [showActivityBar, setShowActivityBar] = React.useState<Checked>(false);
+  const [showPanel, setShowPanel] = React.useState<Checked>(false);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="brutal-normal">Open</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuCheckboxItem checked={showStatusBar} onCheckedChange={setShowStatusBar}>
+          Status Bar
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={showActivityBar}
+          onCheckedChange={setShowActivityBar}
+          disabled
+        >
+          Activity Bar
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem checked={showPanel} onCheckedChange={setShowPanel}>
+          Panel
+        </DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export { DropdownMenuCheckboxes };
--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -5,17 +5,17 @@
 const MODES = {
   light: "light",
   dark: "dark",
-} as const
+} as const;
 
 const THEMES = [
-  {
-    mode: MODES.light,
-    name: "default-light",
-  },
-  {
-    mode: MODES.dark,
-    name: "default-dark",
-  },
+  // {
+  //   mode: MODES.light,
+  //   name: "default-light",
+  // },
+  // {
+  //   mode: MODES.dark,
+  //   name: "default-dark",
+  // },
   {
     mode: MODES.light,
     name: "wine-light",
@@ -24,25 +24,19 @@ const THEMES = [
     mode: MODES.dark,
     name: "wine-dark",
   },
-] as const
+] as const;
 
 const THEME_CONFIG = {
   MODE_CLASS: "dark",
   DATA_ATTRIBUTE: "data-theme",
   CHANGE_EVENT: "theme-change",
   LOCAL_STORAGE_KEY: "theme",
-} as const
+} as const;
 
 // for debugging
 const DEFAULT_THEMES = {
   light: THEMES[0],
   dark: THEMES[1],
-} as const
+} as const;
 
-// for debugging
-const WINE_THEMES = {
-  light: THEMES[2],
-  dark: THEMES[3],
-} as const
-
-export { MODES, THEMES, THEME_CONFIG, DEFAULT_THEMES, WINE_THEMES }
+export { MODES, THEMES, THEME_CONFIG, DEFAULT_THEMES };
