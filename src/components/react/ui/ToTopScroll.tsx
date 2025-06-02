b8aa8455ca8ac87e8a8e36228fe3fbb6722b5193
gao_yangkai <zard200107@gmail.com>
2025-06-02 13:12:30 +0800

    To top scroll

---
+++ src/components/react/ui/ToTopScroll.tsx
@@ -0,0 +1,124 @@
+import { useEffect, useRef, useState } from "react";
+
+import { ArrowUpToLine } from "lucide-react";
+import { SELECTORS } from "@/constants/dom";
+import { cn } from "@/utils/ui/styles";
+import { Button } from "@/components/react/radix-ui/Button";
+
+import type { FC, MouseEvent, RefObject } from "react";
+
+const { SCROLL_TO_TOP_SELECTOR } = SELECTORS;
+
+const fixedClasses = ["opacity-1", "translate-y-0"];
+const hiddenClasses = ["opacity-0", "translate-y-20"];
+
+const showLink = (linkRef: RefObject<HTMLAnchorElement | null>): void => {
+  linkRef.current?.classList.add(...fixedClasses);
+  linkRef.current?.classList.remove(...hiddenClasses);
+};
+
+const hideLink = (linkRef: RefObject<HTMLAnchorElement | null>): void => {
+  linkRef.current?.classList.remove(...fixedClasses);
+  linkRef.current?.classList.add(...hiddenClasses);
+};
+
+const getHalfViewportHeight = (window: Window) => Math.floor(window.innerHeight / 2);
+
+const ToTopScroll: FC = () => {
+  const linkRef = useRef<HTMLAnchorElement>(null);
+  const topRef = useRef<HTMLDivElement>(null);
+  const bottomRef = useRef<HTMLDivElement>(null);
+
+  const [height, setHeight] = useState(getHalfViewportHeight(window));
+
+  useEffect(() => {
+    // ! track them both independently at same time
+    // ! important: must be in this scope, outside of callback()
+    let isAtTop = false;
+    let isAtBottom = false;
+
+    const callback: IntersectionObserverCallback = (entries) => {
+      // entries.length === 1 || 2, count changes when exits viewport
+      entries.forEach((entry) => {
+        if (entry.target === topRef.current) {
+          isAtTop = entry.isIntersecting;
+        }
+
+        if (entry.target === bottomRef.current) {
+          isAtBottom = entry.isIntersecting;
+        }
+      });
+
+      if (linkRef.current) {
+        if (isAtTop || isAtBottom) hideLink(linkRef);
+        else showLink(linkRef);
+      }
+    };
+
+    const intersect = new IntersectionObserver(callback, { threshold: 0 });
+
+    if (topRef.current) intersect.observe(topRef.current);
+    if (bottomRef.current) intersect.observe(bottomRef.current);
+
+    return () => {
+      intersect.disconnect();
+    };
+  }, []);
+
+  // on resize only, vertical...?
+  useEffect(() => {
+    const handleResize = () => {
+      window.requestAnimationFrame(() => setHeight(getHalfViewportHeight(window)));
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleScrollToTop = (event: MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+
+    const anchorElement = document.querySelector(SCROLL_TO_TOP_SELECTOR);
+    if (!anchorElement) return;
+
+    anchorElement.scrollIntoView({ block: "start", behavior: "smooth" });
+  };
+
+  return (
+    <>
+      <div
+        ref={topRef}
+        className="pointer-events-none absolute top-0 w-0"
+        style={{ height: `${height}px` }}
+      />
+      {/* mounted in <body /> in Base layout */}
+      <div
+        ref={bottomRef}
+        className="pointer-events-none absolute bottom-0 w-0"
+        style={{ height: `${height}px` }}
+      />
+      <a
+        ref={linkRef}
+        id="to-top"
+        href={SCROLL_TO_TOP_SELECTOR}
+        onClick={handleScrollToTop}
+        className={cn(
+          // default styles
+          "bg-base-200 border-base-300 fixed right-6 bottom-6 z-10 rounded border p-1",
+          // initial state
+          hiddenClasses,
+          // transition classes
+          "transition-all duration-300"
+        )}
+        aria-label="Scroll to top"
+      >
+        <ArrowUpToLine className="text-content hover:text-links-hover" />
+      </a>
+    </>
+  );
+};
+
+export { ToTopScroll };
