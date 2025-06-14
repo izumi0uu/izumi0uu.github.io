206068ce46e82e76963409f982669e01831818eb
gao_yangkai <zard200107@gmail.com>
2025-06-14 21:03:56 +0800

    memory leak  fix

--- src/components/react/radix-ui/Popover.tsx
+++ src/components/react/radix-ui/Popover.tsx
@@ -18,8 +18,14 @@ const PopoverContent = React.forwardRef<
       ref={ref}
       align={align}
       sideOffset={sideOffset}
+      onOpenAutoFocus={(event) => {
+        // 防止自动聚焦导致的内存泄漏问题
+        event.preventDefault();
+        // 如果props中有自定义的onOpenAutoFocus，则调用它
+        props.onOpenAutoFocus?.(event);
+      }}
       className={cn(
-        "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 border-2 border-black p-4 transition-all outline-none dark:border-white dark:bg-zinc-800",
+        "bg-popover text-popover-foreground z-50 w-72 border-2 border-black p-4 transition-all outline-none data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95 dark:border-white dark:bg-zinc-800",
         className
       )}
       {...props}
